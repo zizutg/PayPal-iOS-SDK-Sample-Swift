da547eae60edfb59a5ae3b86bf05f951c1b170b6
zizutg <zizutg@gmail.com>
2015-09-19 00:45:48 +0900

    Update AppDelegate.swift
    NSDictionary is changed with [NSObject: AnyObject]. Otherwise the didFinishLaunchingWithOptions will have a conflict error. Nice work and thank you

--- PayPalSDK/AppDelegate.swift
+++ PayPalSDK/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         PayPalMobile.initializeWithClientIdsForEnvironments([PayPalEnvironmentProduction: "ID1", PayPalEnvironmentSandbox: "ID2"])
         return true
     }
